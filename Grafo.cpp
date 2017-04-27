d90c2fbf8db5ecfc59f26eb49a94797a4a7fcbbf
carlosmartell97 <carlosmartell97@gmail.com>
2017-04-27 03:08:32 -0500

    now adding all friends of friends to the same Hash Table and looking for collisions there as someone to recommend. It ignores everyone in ignorePeople, and returns the totalInteractions inside each Recommendation with their latest value

--- Grafo.cpp
+++ Grafo.cpp
@@ -1,56 +1,51 @@
 #include "Grafo.h"
 
-float consideracionDirectos = 0.1;
-float consideracionIndirectos = 0.06667;
+// float consideracionDirectos = 0.1;
+float consideracionIndirectos = 0.5;
 
 vector<Recommendation> recommend(Vertice *origin){
   vector<Recommendation> recommendations;
+  vector<Vertice*> toLookForInMap;
   cout << "running recommendations for " << origin->key << "..." << endl;
+  map<string,double> friendsOfFriends;
+  map<string,short> ignorePeople;
   for(int i=0; i<origin->amigos.size(); i++){
-    for(int j=i+1; j<origin->amigos.size(); j++){
-      if(origin->interacciones[i]!=0 && origin->interacciones[j]!=0){
-        // cout << "\t" << origin->amigos[i]->key << " -> " << origin->amigos[j]->key << endl;
-        map<string,int> friendsInI;
-        // cout << "friends of " << origin->amigos[i]->key << ":" << endl;
-        for(int k=0; k<origin->amigos[i]->amigos.size(); k++){
-          // cout << "\t-" << origin->amigos[i]->amigos[k]->key << endl;
-          if(origin->key != origin->amigos[i]->amigos[k]->key)
-            friendsInI[origin->amigos[i]->amigos[k]->key] = k;
-        }
-        // cout << "with friends of " << origin->amigos[j]->key << ":" << endl;
-        for(int k=0; k<origin->amigos[j]->amigos.size(); k++){
-          bool e = friendsInI.find(origin->amigos[j]->amigos[k]->key) != friendsInI.end();
-          // cout << "\t+" << origin->amigos[j]->amigos[k]->key << "<<<" << e << endl;
-
-          if(/*origin->key != origin->amigos[i]->amigos[friendsInI[origin->amigos[j]->amigos[k]->key]]->key &&*/ origin->key != origin->amigos[j]->amigos[k]->key && friendsInI.find(origin->amigos[j]->amigos[k]->key) != friendsInI.end()){
-            // cout << "\tTRUE:" << origin->amigos[j]->amigos[k]->key << endl;
-            cout << "found " << origin->amigos[j]->amigos[k]->key << " through\t" <<
-              origin->key << " -"<<origin->interacciones[i]<<"::"<<origin->interacciones[i]/origin->interaccionesTotal<<"::"<<(origin->interacciones[i]/*/origin->interaccionesTotal*/)*consideracionDirectos<<"-> " << origin->amigos[i]->key << " -"<<origin->amigos[i]->interacciones[friendsInI[origin->amigos[j]->amigos[k]->key]]<<"::"<<flush;
-                cout << origin->amigos[i]->interacciones[friendsInI[origin->amigos[j]->amigos[k]->key]]/origin->amigos[i]->interaccionesTotal<<"::"<<flush;
-                cout<<(origin->amigos[i]->interacciones[friendsInI[origin->amigos[j]->amigos[k]->key]]/*/origin->amigos[i]->interaccionesTotal*/)*consideracionIndirectos<<"-> " << origin->amigos[i]->amigos[friendsInI[origin->amigos[j]->amigos[k]->key]]->key << "\n\t\tand \t" <<flush;
-              cout<<origin->key << " -"<<origin->interacciones[j]<<"::"<<origin->interacciones[j]/origin->interaccionesTotal<<"::"<<(origin->interacciones[j]/*/origin->interaccionesTotal*/)*consideracionDirectos<<"-> " << origin->amigos[j]->key << " -"<<origin->amigos[j]->interacciones[k]<<"::"<<origin->amigos[j]->interacciones[k]/origin->amigos[j]->interaccionesTotal<<"::"<<flush;
-                cout<<(origin->amigos[j]->interacciones[k]/*/origin->amigos[j]->interaccionesTotal*/)*consideracionIndirectos<<"-> " << origin->amigos[j]->amigos[k]->key << endl;
-          double interactions = 0;
+    ignorePeople[origin->amigos[i]->key] = 0;
+  }
+  for(int i=0; i<origin->amigos.size(); i++){
+    if(origin->interacciones[i] != 0){
+      cout << origin->amigos[i]->key << endl;
+      for(int j=0; j<origin->amigos[i]->amigos.size(); j++){
+        if(origin->key != origin->amigos[i]->amigos[j]->key){
+          cout << "\t" << origin->amigos[i]->amigos[j]->key << endl;
+          if(ignorePeople.find(origin->amigos[i]->amigos[j]->key) == ignorePeople.end() && friendsOfFriends.find(origin->amigos[i]->amigos[j]->key) != friendsOfFriends.end()){
+            cout << "\tCOLLISION " << origin->amigos[i]->amigos[j]->key << endl;
+            toLookForInMap.push_back(origin->amigos[i]->amigos[j]);
+            ignorePeople[origin->amigos[i]->amigos[j]->key] = 0;
+          }
+          cout << "\t\t" << origin->key << "-" << origin->interacciones[i] << "::" << origin->interacciones[i]/origin->interaccionesTotal << "->" << flush;
+          cout << origin->amigos[i]->key << "-" << origin->amigos[i]->interacciones[j] << "::" << flush;
+            cout << origin->amigos[i]->interacciones[j]/origin->amigos[i]->interaccionesTotal << "::" << flush;
+            cout << (origin->amigos[i]->interacciones[j]/origin->amigos[i]->interaccionesTotal)*consideracionIndirectos << flush;
+            cout << "->" << origin->amigos[i]->amigos[j]->key << endl;
           if(origin->interaccionesTotal != 0)
-            interactions += (origin->interacciones[i]/*/origin->interaccionesTotal*/)*consideracionDirectos + (origin->interacciones[j]/*/origin->interaccionesTotal*/)*consideracionDirectos;
+            friendsOfFriends[origin->amigos[i]->amigos[j]->key] += origin->interacciones[i]/origin->interaccionesTotal;
           if(origin->amigos[i]->interaccionesTotal != 0)
-            interactions += (origin->amigos[i]->interacciones[friendsInI[origin->amigos[j]->amigos[k]->key]]/*/origin->amigos[i]->interaccionesTotal*/)*consideracionIndirectos;
-          if(origin->amigos[j]->interaccionesTotal != 0)
-            interactions += (origin->amigos[j]->interacciones[k]/*/origin->amigos[j]->interaccionesTotal*/)*consideracionIndirectos;
-
-          // interactions += origin->interacciones[i] + origin->amigos[i]->interacciones[k] + origin->interacciones[j] + origin->amigos[j]->interacciones[l];
-          Recommendation *found = new Recommendation(origin->amigos[i]->amigos[friendsInI[origin->amigos[j]->amigos[k]->key]],interactions+2);
-          recommendations.push_back(*found);
-          delete found;
-        } /*else {
-          cout << "nope..." << endl;
-        }*/
+            friendsOfFriends[origin->amigos[i]->amigos[j]->key] += (origin->amigos[i]->interacciones[j]/origin->amigos[i]->interaccionesTotal)*consideracionIndirectos;
+          friendsOfFriends[origin->amigos[i]->amigos[j]->key] += 1;
+          cout << "\t\tbucket:" << friendsOfFriends[origin->amigos[i]->amigos[j]->key] << endl;
         }
-        friendsInI.clear();
-        cout << "--------------------" << endl;
       }
     }
-    // if(i+1 == origin->amigos.size()) cout << "\t" << origin->amigos[i]->key << " -> NONE" << endl;
   }
+  cout << "size: " << toLookForInMap.size() << endl;
+  for(int i=0; i<toLookForInMap.size(); i++){
+    Recommendation *found = new Recommendation(toLookForInMap[i],friendsOfFriends[toLookForInMap[i]->key]);
+    recommendations.push_back(*found);
+    delete found;
+  }
+  toLookForInMap.clear();
+
+  cout << "----------------------" << endl;
   return recommendations;
 }
